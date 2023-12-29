9e16d60a7c51e4fc650f45e32b59de895cdcb7c5
Zeeshan <Zeeshan16@outlook.com>
2023-12-29 15:29:14 +0000

    Commiting sample-app files.

--- HR_app.py
+++ HR_app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 HR = Flask(__name__)
 
 @HR.route("/")
 def main():
-	return"HR department has access to the marketing department " + "\n"
+	return render_template("index.html") + "HR wants access to Marketing department "
 
 if __name__ == "__main__":
      HR.run(host="0.0.0.0", port=5000)
